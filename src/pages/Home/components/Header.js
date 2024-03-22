130b57321d1be8df365ff2d613b62fb8aa3e93fc
Jessica Coelho <jessicac.conceicao1@gmail.com>
2024-03-22 09:09:27 -0300

    Inicial Code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header className='header'>
+               <div className="toolbar">
+                <div>
+                    <span> Rede Levva </span>
+                </div>
+                <div>
+                    <button> novo post </button>
+                    <span> Img 1</span>
+                    <span> img 2 </span>
+                </div>
+               </div>
+            </header>
+    )
+}
+
+export default Header;
